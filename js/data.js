934679b8b7f24972a3f8bea9e324c98beaa498e1
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2021-10-24 15:10:23 +0200

    Add changes to navbar buttons and hamburger and small edit in index.html

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
-const slideList = [
-  {
-    img: "images/bookShelves.jpg",
-    text: "Największy wybór",
-  }, 
-  {
-    img: "images/pileOfBooks.jpg",
-    text: "Niskie ceny",
-  },
-  { 
-    img: "images/magicOfReading.jpg",
-    text: "Wciągające historie",
-  }
-]
+// const slideList = [
+//   {
+//     img: "images/bookShelves.jpg",
+//     text: "Największy wybór",
+//   }, 
+//   {
+//     img: "images/pileOfBooks.jpg",
+//     text: "Niskie ceny",
+//   },
+//   { 
+//     img: "images/magicOfReading.jpg",
+//     text: "Wciągające historie",
+//   }
+// ]
--- js/script.js
+++ js/script.js
@@ -1,34 +1,91 @@
 const navbarButtonListener = () => {
   const dropdownMenu = document.querySelector('.dropdown-menu');
+  const dropdown = document.querySelector('.dropdown');
+  // const hamburger = document.querySelector('.navbar-hamburger');
   const navbarLinksArray = [...document.querySelectorAll('.navbar-nav li')];
+  console.log(navbarLinksArray);
 
-  for(let i=0; i<navbarLinksArray.length; i++) {
-    navbarLinksArray[i].addEventListener('click', () => {
-      navbarLinksArray[i].classList.toggle('show');
-      i == 0 ? dropdownMenu.classList.toggle('show') : null;  
-      for(let j=0; j<navbarLinksArray.length; j++) {
-        if(j!=i) {
-          navbarLinksArray[j].classList.remove('show');
-          j == 0 ? dropdownMenu.classList.remove('show') : null;  
-        }
-      }
-    });
+  const showRemover = (list) => {
+    for(li of list) {
+      li.classList.remove('show');
+    }
   }
 
-  document.querySelector('.navbar-hamburger').addEventListener('click', () => {
-    document.querySelector('.navbar-nav').classList.toggle('active');
-    document.querySelector('.navbar-buttons').classList.toggle('active');
+  document.body.addEventListener('click', function(e){
+    if(!e.target.classList.contains('menu')){
+      const showedList = [...document.querySelectorAll('.show')];
+      showRemover(showedList);
+      console.log(e.target);
+    } else {
+      const showedList = [...document.querySelectorAll('.show')];
+      if(e.target.classList.contains('navbar-links') || e.target.classList.contains('dropdown')) {
+        dropdown.classList.toggle('show');
+        dropdownMenu.classList.toggle('show');
+        showRemover(showedList);
+      } else if(e.target.classList.contains('navbar-link')) {
+        console.log(e.target);
+        e.target.classList.toggle('show');
+        showRemover(showedList);
+      } else if(e.target.classList.contains('fas') || e.target.classList.contains('hamburger')) {
+        console.log(e.target);
+        document.querySelector('.navbar-nav').classList.toggle('active');
+        document.querySelector('.navbar-buttons').classList.toggle('active');
 
-    dropdownMenu.classList.remove('show');
-
-    for(navbarLink of navbarLinksArray) {
-      navbarLink.classList.remove('show');
+        showRemover(showedList);
+      }
+    
     }
-  });
+  })
 }
 
 const sliderHandler = () => {
-  
+  let currentSlide = null;
+  let prevSlide = null;
+  let nextSlide = null;
+  const leftBtn = document.querySelector('.left');
+  const rightBtn = document.querySelector('.right');
+  const slideList = [...document.querySelectorAll('.slider-item')];
+
+  const currentSlideCheck = () => {
+    slideList.forEach((elem, id) => {
+      if(elem.classList.contains('current')) {
+        currentSlide = id;
+        prevSlide = currentSlide - 1;
+        nextSlide = currentSlide + 1
+      }
+      if(prevSlide < 0) {
+        prevSlide = slideList.length -1;
+      } else if(nextSlide > slideList.length -1) {
+        nextSlide = 0;
+      }
+    })
+  }
+
+
+  leftBtn.addEventListener('click', () => {
+    currentSlideCheck();
+    console.log(currentSlide);
+
+    slideList.forEach(slide => slide.classList.remove('next'));
+
+    slideList[currentSlide].classList.add('next');
+    slideList[currentSlide].classList.remove('current');
+    slideList[prevSlide].classList.add('current');
+    console.log(leftBtn);
+  })
+
+  rightBtn.addEventListener('click', () => {
+    currentSlideCheck();
+    console.log(currentSlide);
+    
+    slideList.forEach(slide => slide.classList.remove('prevHide'));
+    slideList[currentSlide].classList.add('prevHide');
+    slideList[currentSlide].classList.remove('current');
+    slideList[nextSlide].classList.add('current');
+    console.log(rightBtn);
+  })
+
 }
 
+sliderHandler()
 navbarButtonListener()
