b0f4fc1f77fa1ec8608225d0d0a44da94a1ec7a5
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2021-12-02 12:08:54 +0100

    Add setting.js and changes in scripts.js document.query selectors

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,39 @@
 'use strict';
 import dataSources from './data.js';
+import {classesToAdd, select, templates} from './settings.js';
 
 const appStart = () => {
-
-  const templates = {
-    autorsList: Handlebars.compile(document.querySelector('#template-autors-list').innerHTML),
-    booksList: Handlebars.compile(document.querySelector('#template-books-list').innerHTML),
-    titlesProductInCart: Handlebars.compile(document.querySelector('#template-titles-list').innerHTML)
-  };
-
   const buttonListener = () => {
-    const booksAnchor = [...document.querySelectorAll('a[data-target*="books"]')];
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    const dropdown = document.querySelector('.dropdown');
-    const accountBtn = document.querySelector('.fa-user-alt');
-    const modal = document.querySelector('.modal-wrap');
-    const modalCloseBtn = document.querySelector('.modalClose');
-    const modalBtn = document.querySelector('.modal');
-    const modalRead = document.querySelector('.modal-read');
-    const cart = document.querySelector('.cart');
+    const booksAnchor = [...document.querySelectorAll(select.anchors.booksList)];
+    const dropdownMenu = document.querySelector(select.items.dropdownMenu);
+    const dropdown = document.querySelector(select.items.dropdown);
+    const accountBtn = document.querySelector(select.items.user);
+    const modal = document.querySelector(select.modals.modalWrap);
+    const modalCloseBtn = document.querySelector(select.modals.modalClose);
+    const modalBtn = document.querySelector(select.modals.modal);
+    const modalRead = document.querySelector(select.modals.modalRead);
+    const cart = document.querySelector(select.items.cart);
     let lastTarget = null;
 
     booksAnchor.forEach(anchor => {
       anchor.addEventListener('click', () => {
         window.scrollTo({
-          top: document.querySelector('.books').offsetTop - 64,
+          top: document.querySelector(select.items.books).offsetTop - 64,
           behavior: 'smooth'
         });
       });
     });
 
-    document.querySelector('li[data-target*="recommended"]').addEventListener('click', () => {
+    document.querySelector(select.anchors.recommended).addEventListener('click', () => {
       window.scrollTo({
-        top: document.querySelector('.second-image').offsetTop - 64,
+        top: document.querySelector(select.items.secondImage).offsetTop - 64,
         behavior: 'smooth'
       });
     });
 
-    document.querySelector('li[data-target*="contact"]').addEventListener('click', () => {
+    document.querySelector(select.anchors.contact).addEventListener('click', () => {
       window.scrollTo({
-        top: document.querySelector('.order-3').offsetTop - 64,
+        top: document.querySelector(select.items.order3).offsetTop - 64,
         behavior: 'smooth'
       });
     });
@@ -47,65 +41,68 @@ const appStart = () => {
     const blurToggler = () => {
       [...document.body.children].forEach(elem => {
         if(elem.classList != ''  || elem.localName == 'footer') {
-          elem.classList.toggle('blur');
+          elem.classList.toggle(classesToAdd.blur);
         }
       });
-      modal.classList.remove('blur');
+      modal.classList.remove(classesToAdd.blur);
     };
     
     accountBtn.addEventListener('click', () => {
       blurToggler();
-      modal.classList.add('active');
-      modalBtn.classList.add('active');
+      modal.classList.add(classesToAdd.active);
+      modalBtn.classList.add(classesToAdd.active);
     });
   
     modalCloseBtn.addEventListener('click', () => {
       blurToggler();
-      modal.classList.remove('active');
-      modalBtn.classList.remove('active');
-      modalRead.classList.remove('active');
+      modal.classList.remove(classesToAdd.active);
+      modalBtn.classList.remove(classesToAdd.active);
+      modalRead.classList.remove(classesToAdd.active);
     });
   
     const showRemover = (list) => {
       for(let li of list) {
-        li.classList.remove('show');
+        li.classList.remove(classesToAdd.show);
       }
     };
   
     document.body.addEventListener('click', function(e){
-      if(!e.target.classList.contains('menu')){
+      if(!e.target.classList.contains(classesToAdd.menu)){
         e.target.preventDefault;
-        const showedList = [...document.querySelectorAll('.show')];
-        const booksList = [...document.querySelectorAll('.box')];
+        const showedList = [...document.querySelectorAll(select.list.show)];
+        const booksList = [...document.querySelectorAll(select.list.box)];
   
         showRemover(showedList);
   
-        if(e.target.classList.contains('read-button')) {
+        if(e.target.classList.contains(classesToAdd.read)) {
           e.target.preventDefault;
           blurToggler();
-          modal.classList.add('active');
-          modalRead.classList.add('active');
+          modal.classList.add(classesToAdd.active);
+          modalRead.classList.add(classesToAdd.active);
           const id =  e.target.getAttribute('data-id');
           const title = dataSources.booksList[id].title;
           const discribe = dataSources.booksList[id].description;
           const amount = dataSources.booksList[id].amount;
           const price = dataSources.booksList[id].price;
   
-          document.querySelector('.modal-read h2').innerHTML = title;
-          document.querySelector('.modal-read p').innerHTML = discribe;
-          document.querySelector('.add-button').setAttribute('data-id', id);
+          document.querySelector(select.modals.title).innerHTML = title;
+          document.querySelector(select.modals.describe).innerHTML = discribe;
+          console.log(document.querySelector(select.buttons.add).setAttribute('data-id', id));
+          document.querySelector(select.buttons.add).setAttribute('data-id', id);
   
+          // console.log('i');
+
           if(dataSources.booksList[id].amount == 0) {
-            document.querySelector('.add-button').classList.add('lack');
+            document.querySelector(select.buttons.add).classList.add(classesToAdd.lack);
           } else {
-            document.querySelector('.add-button').classList.remove('lack');
+            document.querySelector(select.buttons.add).classList.remove(classesToAdd.lack);
           }
-          document.querySelector('.stock').innerHTML = `In stock: ${amount}`;
-          document.querySelector('.price').innerHTML = `${price} PLN`;
+          document.querySelector(select.cart.stock).innerHTML = `In stock: ${amount}`;
+          document.querySelector(select.cart.price).innerHTML = `${price} PLN`;
         } else if(e.target == lastTarget) {
           e.target.preventDefault;
           booksList.forEach(box => {
-            box.classList.remove('hide');
+            box.classList.remove(classesToAdd.hide);
             lastTarget = null;
           });
         } else if(e.target.getAttribute('data-autor')) {
@@ -113,9 +110,9 @@ const appStart = () => {
           booksList.filter(box => {
             const autor = box.getAttribute('data-autor');
             if(autor.includes(`${e.target.getAttribute('data-autor')}`)) {
-              box.classList.remove('hide');
+              box.classList.remove(classesToAdd.hide);
             } else {
-              box.classList.add('hide');
+              box.classList.add(classesToAdd.hide);
             }
           });
   
@@ -124,40 +121,40 @@ const appStart = () => {
           booksList.filter(box => {
             const type = box.getAttribute('data-type');
             if(type.includes(`${e.target.getAttribute('data-type')}`)) {
-              box.classList.remove('hide');
+              box.classList.remove(classesToAdd.hide);
             } else {
-              box.classList.add('hide');
+              box.classList.add(classesToAdd.hide);
             }
           });
-        } else if((e.target.classList.contains('add-button')) & (!e.target.classList.contains('lack'))) {
+        } else if((e.target.classList.contains(classesToAdd.addBtn)) & (!e.target.classList.contains(classesToAdd.lack))) {
           addToCart(e.target.getAttribute('data-id'));
-        } else if(e.target.classList.contains('fa-backspace')) {
+        } else if(e.target.classList.contains(select.buttons.removeBtn)) {
           removeFromCart(e.target.parentNode.innerText);
         }
       } else {
         e.target.preventDefault;
-        const showedList = [...document.querySelectorAll('.show')];
-        if(e.target.classList.contains('navbar-links') || e.target.classList.contains('dropdown')) {
-          dropdown.classList.toggle('show');
-          dropdownMenu.classList.toggle('show');
-          cart.classList.remove('active');
+        const showedList = [...document.querySelectorAll(select.list.show)];
+        if(e.target.classList.contains(select.items.navbarLinks) || e.target.classList.contains(classesToAdd.dropdown)) {
+          dropdown.classList.toggle(classesToAdd.show);
+          dropdownMenu.classList.toggle(classesToAdd.show);
+          document.querySelector(select.buttons.cartBtn).classList.remove(classesToAdd.active);
+          cart.classList.remove(classesToAdd.active);
           showRemover(showedList);
-        } else if(e.target.classList.contains('navbar-link')) {
-          console.log(e.target);
-          e.target.classList.toggle('show');
+        } else if(e.target.classList.contains(select.items.navbarLink)) {
+          e.target.classList.toggle(classesToAdd.show);
           showRemover(showedList);
-        } else if(e.target.classList.contains('hamburger')) {
-          document.querySelector('.navbar-nav').classList.toggle('active');
-          document.querySelector('.navbar-buttons').classList.toggle('active');
+        } else if(e.target.classList.contains(select.items.hamburger)) {
+          document.querySelector(select.items.navbarNav).classList.toggle(classesToAdd.active);
+          document.querySelector(select.buttons.navbarButtons).classList.toggle(classesToAdd.active);
           showRemover(showedList);
-        } else if((e.target.classList.contains('fa-shopping-cart')) || (e.target.classList.contains('amount'))) {
-          document.querySelector('.cartBtn').classList.toggle('active');
-          cart.classList.toggle('active');
+        } else if((e.target.classList.contains(select.items.shopingCart)) || (e.target.classList.contains(classesToAdd.amount))) {
+          document.querySelector(select.buttons.cartBtn).classList.toggle(classesToAdd.active);
+          cart.classList.toggle(classesToAdd.active);
           showRemover(showedList);
-        } else if(e.target.classList.contains('fa-user-alt')) {
-          e.target.parentNode.classList.toggle('show');
+        } else if(e.target.classList.contains(classesToAdd.user)) {
+          e.target.parentNode.classList.toggle(classesToAdd.show);
           showRemover(showedList);
-        } 
+        }
       }
     });
   };
@@ -167,8 +164,8 @@ const appStart = () => {
     let prev = null;
     let next = null;
     let didSlideEnd = true;
-    const leftBtn = document.querySelector('.left');
-    const rightBtn = document.querySelector('.right');
+    const leftBtn = document.querySelector(select.buttons.left);
+    const rightBtn = document.querySelector(select.buttons.right);
   
     const currentSlideCheck = (slideAmount, current) => {
       if(current == slideAmount - 1) {
@@ -185,25 +182,25 @@ const appStart = () => {
   
     const changeSlideOnNext = () => {
       didSlideEnd = false;
-      const slideList = [...document.querySelectorAll('.slider-item')];
+      const slideList = [...document.querySelectorAll(select.items.slides)];
       slideList.forEach((elem, id) => {
-        if(elem.classList.contains('current')) {
+        if(elem.classList.contains(classesToAdd.current)) {
           current = id;
         }
       });
   
       currentSlideCheck(slideList.length, current);
   
-      slideList[current].classList.add('prev');
-      slideList[prev].classList.remove('prev');
-      slideList[prev].classList.add('hide');
-      slideList[prev].classList.add('next');
-      slideList[next].classList.add('current');
-      slideList[next].classList.remove('next');
+      slideList[current].classList.add(classesToAdd.prev);
+      slideList[prev].classList.remove(classesToAdd.prev);
+      slideList[prev].classList.add(classesToAdd.hide);
+      slideList[prev].classList.add(classesToAdd.next);
+      slideList[next].classList.add(classesToAdd.current);
+      slideList[next].classList.remove(classesToAdd.next);
   
       setTimeout( function() {
-        slideList[current].classList.remove('current');
-        slideList[prev].classList.remove('hide');
+        slideList[current].classList.remove(classesToAdd.current);
+        slideList[prev].classList.remove(classesToAdd.hide);
         didSlideEnd = true;
       }, 600);
     };
@@ -213,25 +210,25 @@ const appStart = () => {
     leftBtn.addEventListener('click', () => {
       if(didSlideEnd){
         didSlideEnd = false;
-        const slideList = [...document.querySelectorAll('.slider-item')];
+        const slideList = [...document.querySelectorAll(select.items.slides)];
         slideList.forEach((elem, id) => {
-          if(elem.classList.contains('current')) {
+          if(elem.classList.contains(classesToAdd.current)) {
             current = id;
           }
         });
         
         currentSlideCheck(slideList.length, current);
   
-        slideList[current].classList.add('next');
-        slideList[prev].classList.add('current');
-        slideList[next].classList.remove('next');
-        slideList[next].classList.add('hide');
-        slideList[next].classList.add('prev');
-        slideList[prev].classList.remove('prev');
+        slideList[current].classList.add(classesToAdd.next);
+        slideList[prev].classList.add(classesToAdd.current);
+        slideList[next].classList.remove(classesToAdd.next);
+        slideList[next].classList.add(classesToAdd.hide);
+        slideList[next].classList.add(classesToAdd.prev);
+        slideList[prev].classList.remove(classesToAdd.prev);
   
         setTimeout( function() {
-          slideList[current].classList.remove('current');
-          slideList[next].classList.remove('hide');
+          slideList[current].classList.remove(classesToAdd.current);
+          slideList[next].classList.remove(classesToAdd.hide);
           didSlideEnd = true;
         }, 600);
         clearInterval(regularSlideChange);
@@ -251,28 +248,28 @@ const appStart = () => {
   };
   
   const autorsListCreate = autors => {
-    const autorsList = document.querySelector('.autors-list');
+    const autorsList = document.querySelector(select.list.autors);
     const generatedHTMLAutors = templates.autorsList({autors});
     autorsList.insertAdjacentHTML('beforeend', generatedHTMLAutors);
   };
   
   const booksListCreate = books => {
-    const booksList = document.querySelector('.books-list');
+    const booksList = document.querySelector(select.list.books);
     const generatedHTMLBooks = templates.booksList({books});
     booksList.insertAdjacentHTML('beforeend', generatedHTMLBooks);
   };
   
   const cartAktualization = (elem, type) => {
-    const addButtonList = document.querySelectorAll('.add-button');
+    const addButtonList = document.querySelectorAll(select.buttons.add);
     const amountProduct = dataSources.cart.amountOfProduct;
-    document.querySelector('.amount').innerHTML = amountProduct;
-    document.getElementById('amountOfProduct').innerHTML = `Amount of product: ${amountProduct}`;
-    document.querySelector('.stock').innerHTML = `In stock: ${dataSources.booksList[elem].amount}`;
-    document.getElementById('subtotal').innerHTML = `Subtotal: ${dataSources.cart.price.toFixed(2)}`;
-    document.getElementById('deliverFee').innerHTML = `Deliverdy cost: ${dataSources.cart.deliveryFee}`;
-    document.getElementById('totalPrice').innerHTML = `Total price: ${dataSources.cart.totalPrice.toFixed(2)}`;
+    document.querySelector(select.cart.amount).innerHTML = amountProduct;
+    document.getElementById(select.cart.product).innerHTML = `Amount of product: ${amountProduct}`;
+    document.querySelector(select.cart.stock).innerHTML = `In stock: ${dataSources.booksList[elem].amount}`;
+    document.getElementById(select.cart.subtotal).innerHTML = `Subtotal: ${dataSources.cart.price.toFixed(2)}`;
+    document.getElementById(select.cart.delivery).innerHTML = `Deliverdy cost: ${dataSources.cart.deliveryFee}`;
+    document.getElementById(select.cart.total).innerHTML = `Total price: ${dataSources.cart.totalPrice.toFixed(2)}`;
   
-    const titlesOfProduct = document.getElementById('titlesOfProducts');
+    const titlesOfProduct = document.getElementById(select.cart.productList);
     if(type == 'add') {
       const titleData = {id: elem, title: dataSources.booksList[elem].title};
       const generatedHTMLTitles = templates.titlesProductInCart(titleData);
@@ -289,13 +286,13 @@ const appStart = () => {
     if(dataSources.booksList[elem].amount == 0) {
       addButtonList.forEach(btn => {
         if(btn.getAttribute('data-id') == elem) {
-          btn.classList.add('lack');
+          btn.classList.add(classesToAdd.lack);
         }
       });
     } else {
       addButtonList.forEach(btn => {
         if(btn.getAttribute('data-id') == elem) {
-          btn.classList.remove('lack');
+          btn.classList.remove(classesToAdd.lack);
         }
       });
     }
@@ -336,6 +333,7 @@ const appStart = () => {
     } else {
       alert('This book is already in cart');
     }
+    console.log(dataSources.cart);
   };
   
   const removeFromCart = title => {
