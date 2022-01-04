2730ffdfb2df2010e91dc0b3017dec54ea975f72
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2022-01-04 17:44:05 +0100

    Change url in settings.js

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -73,7 +73,7 @@ export const select = {
 
 export const settings = {
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     booksList: 'booksList',
     autorsList: 'autorsList',
     cart: 'cart',
